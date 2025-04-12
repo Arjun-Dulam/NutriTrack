2ee5dae6acb76ef8e4291b8606454d5a4aec6b68
adulam <adulam3@gatech.edu>
2025-04-11 23:16:39 -0400

    changePassword works with correct info, need to test more and also improve html

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('signup/', views.signup, name='accounts.signup'),
     path('login/', views.login, name='accounts.login'),
     path('logout/', views.logout, name='accounts.logout'),
+    path('changePassword/', views.changePassword, name='accounts.changePassword'),
 ]
