baf46437d2ae6c83c644dc46927f95b1fd82452e
cnrbd <puong23@ssis.edu.vn>
2025-04-22 17:59:24 -0400

    finish

--- profiles/forms.py
+++ profiles/forms.py
@@ -9,26 +9,37 @@ DIETARY_RESTRICTIONS_CHOICES = [
 ]
 
 class UserProfileForm(forms.ModelForm):
+    username = forms.CharField(required=True)
+
     dietary_restrictions = forms.MultipleChoiceField(
         choices=DIETARY_RESTRICTIONS_CHOICES,
         widget=forms.CheckboxSelectMultiple,
         required=False,
     )
+
     class Meta:
         model = UserProfile
         fields = ['height_cm', 'weight_kg', 'sex', 'activity_level', 'dietary_restrictions']
-        widgets = {
-            'sex': forms.Select(attrs={'class': 'form-control', 'required': 'required'}),
-            'activity_level': forms.Select(attrs={'class': 'form-control', 'required': 'required'}),
-        }
 
     def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
-        # If editing, split the stored string into a list for the multiple select field.
+        if user:
+            self.fields['username'].initial = user.username
+
         if self.instance and self.instance.dietary_restrictions:
             self.initial['dietary_restrictions'] = self.instance.dietary_restrictions.split(',')
 
+    def save(self, commit=True):
+        profile = super().save(commit=False)
+        # Save the user’s username outside of the profile model
+        if self.cleaned_data.get('username'):
+            self.instance.user.username = self.cleaned_data['username']
+            self.instance.user.save()
+        if commit:
+            profile.save()
+        return profile
+
     def clean_dietary_restrictions(self):
         data = self.cleaned_data.get('dietary_restrictions')
-        # Join the list using commas (or any delimiter of your choice)
         return ','.join(data)
