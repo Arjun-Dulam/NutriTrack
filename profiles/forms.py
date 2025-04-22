6c862d8e4a88d37c69ad9d7d538db1e487c5c394
adulam <adulam3@gatech.edu>
2025-04-22 12:54:08 -0400

    edited dietary restrictions and html

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,12 +1,34 @@
 from django import forms
 from .models import UserProfile
 
+DIETARY_RESTRICTIONS_CHOICES = [
+    ('vegetarian', 'Vegetarian'),
+    ('allergic_to_seafood', 'Allergic to Seafood'),
+    ('peanuts', 'Allergic to Peanuts'),
+    ('lactose_intolerant', 'Lactose Intolerant'),
+]
+
 class UserProfileForm(forms.ModelForm):
+    dietary_restrictions = forms.MultipleChoiceField(
+        choices=DIETARY_RESTRICTIONS_CHOICES,
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
     class Meta:
         model = UserProfile
         fields = ['height_cm', 'weight_kg', 'sex', 'dietary_restrictions', 'activity_level']
         widgets = {
-            'dietary_restrictions': forms.Textarea(attrs={'rows': 2, 'class': 'form-control'}),
             'sex': forms.Select(attrs={'class': 'form-control', 'required': 'required'}),
             'activity_level': forms.Select(attrs={'class': 'form-control', 'required': 'required'}),
         }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # If editing, split the stored string into a list for the multiple select field.
+        if self.instance and self.instance.dietary_restrictions:
+            self.initial['dietary_restrictions'] = self.instance.dietary_restrictions.split(',')
+    
+    def clean_dietary_restrictions(self):
+        data = self.cleaned_data.get('dietary_restrictions')
+        # Join the list using commas (or any delimiter of your choice)
+        return ','.join(data)
