d40000e412c592ea45ab54ad4aea219a1870a4b0
adulam <adulam3@gatech.edu>
2025-04-21 22:41:46 -0400

    acitivty level shows up

--- profiles/forms.py
+++ profiles/forms.py
@@ -4,9 +4,9 @@ from .models import UserProfile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['height_cm', 'weight_kg', 'sex', 'dietary_restrictions', 'water_intake_liters', 'calories_burned']
+        fields = ['height_cm', 'weight_kg', 'sex', 'dietary_restrictions', 'activity_level']
         widgets = {
-            'dietary_restrictions': forms.Textarea(attrs={'rows': 2}),
+            'dietary_restrictions': forms.Textarea(attrs={'rows': 2, 'class': 'form-control'}),
+            'sex': forms.Select(attrs={'class': 'form-control', 'required': 'required'}),
+            'activity_level': forms.Select(attrs={'class': 'form-control'}),
         }
-        
-        
