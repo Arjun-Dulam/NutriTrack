a8508f6dc744b17a63440a868c36247bb02c00f1
cnrbd <puong23@ssis.edu.vn>
2025-04-24 15:30:24 -0400

    add groq api

--- NutriTrack/settings.py
+++ NutriTrack/settings.py
@@ -126,4 +126,5 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+GROQ_API_KEY ="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"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- home/views.py
+++ home/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, redirect
+from groq import Groq
+from django.conf import settings
+
+client = Groq(
+    api_key=settings.GROQ_API_KEY,
+)
+
 
 # Create your views here.
 
+
 def index(request):
-    return render(request,'home/index.html')
+    return render(request,'home/index.html', {'joke': llm_call()})
+
+    # return render(request,'home/index.html' )
 
 def about(request):
     return render(request, 'home/about.html')
@@ -13,3 +23,18 @@ def contact(request):
 
 def services(request):
     return render(request, 'home/services.html')
+
+
+def llm_call():
+    chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "tell me a joke",
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+    stream=False,
+    )
+
+    return chat_completion.choices[0].message.content
