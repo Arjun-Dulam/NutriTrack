5507279f7cf273f68e4c06ad4acba06c3500ba10
adulam <adulam3@gatech.edu>
2025-04-24 10:53:11 -0400

    filtering based on dietary preferences works

--- food/views.py
+++ food/views.py
@@ -1,28 +1,67 @@
 from django.shortcuts import render
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from profiles.models import UserProfile
 import requests
 
-# Create your views here.
+@login_required
 def food_list(request):
     query = request.GET.get('query', '').strip()  # Get the query and strip whitespace
     if not query:  # If no query is provided
         return render(request, 'food/list.html', {'message': 'Search for your favorite foods!'})
 
+    # Get the user's dietary restrictions
+    try:
+        user_profile = UserProfile.objects.get(user=request.user)
+        restrictions_str = user_profile.dietary_restrictions or ""
+    except UserProfile.DoesNotExist:
+        restrictions_str = ""  # Default to no restrictions if profile doesn't exist
+
+    # Parse dietary restrictions into a list
+    restrictions_list = [r.strip() for r in restrictions_str.split(',') if r.strip()]
+
+    # Map restrictions to Spoonacular API parameters
+    diet_param = None
+    intolerances_param = []
+
+    if 'vegetarian' in restrictions_list:
+        diet_param = 'vegetarian'
+        restrictions_list.remove('vegetarian')
+
+    # Map remaining restrictions to Spoonacular intolerances
+    intolerance_mapping = {
+        'allergic_to_seafood': ['Seafood', 'Shellfish'],
+        'peanuts': ['Peanut'],
+        'lactose_intolerant': ['Dairy'],
+    }
+    for restriction in restrictions_list:
+        if restriction in intolerance_mapping:
+            intolerances_param.extend(intolerance_mapping[restriction])
+
+    # Prepare API request
     api_key = settings.SPOONACULAR_API_KEY
-    url = f"https://api.spoonacular.com/food/menuItems/search"
+    url = "https://api.spoonacular.com/recipes/complexSearch"
     params = {
         'query': query,
-        'number': 5,
+        'number': 50,
         'apiKey': api_key,
+        'addRecipeInformation': True,  # Include detailed recipe info
     }
-    
+
+    if diet_param:
+        params['diet'] = diet_param
+    if intolerances_param:
+        params['intolerances'] = ','.join(intolerances_param)
+
+    # Make the API request
     response = requests.get(url, params=params)
     if response.status_code != 200:
         error_message = response.json().get('message', 'An error occurred while fetching data.')
         return render(request, 'food/list.html', {'error': error_message, 'query': query})
-    
-    data = response.json()
-    food_items = data.get('menuItems', [])
 
-    # No need to handle images anymore
+    # Parse the API response
+    data = response.json()
+    food_items = data.get('results', [])
+
+    # Render the results in the template
     return render(request, 'food/list.html', {'food_items': food_items, 'query': query})
