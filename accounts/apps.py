9a943f2ae54c34bd1f6f049287b8b810ff761018
nayavw <nwooldridge3@gatech.edu>
2025-04-27 19:01:47 -0400

    accounts

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
 
+# Configuration class for the 'accounts' app.
 class AccountsConfig(AppConfig):
+    # Defines the type of primary key to use for models in this app.
     default_auto_field = "django.db.models.BigAutoField"
+
+    # The name of the app. Django uses this to locate app files.
     name = "accounts"
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from . import views
+
+# URL patterns for the accounts app.
 urlpatterns = [
+    # Route for user signup page.
     path('signup/', views.signup, name='accounts.signup'),
+
+    # Route for user login page.
     path('login/', views.login, name='accounts.login'),
+
+    # Route for user logout functionality.
     path('logout/', views.logout, name='accounts.logout'),
+
+    # Route for user password change page.
     path('changePassword/', views.changePassword, name='accounts.changePassword'),
 ]
